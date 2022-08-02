42e0d27712f96f7c353d09615309eada8d973640
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2022-08-02 22:34:51 +0430

    first commit

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    public function register(Request $request)
+    {
+        $post_data = $request->validate([
+                'name'=>'required|string',
+                'email'=>'required|string|email|unique:users',
+                'password'=>'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $post_data['name'],
+            'email' => $post_data['email'],
+            'password' => Hash::make($post_data['password']),
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+}
